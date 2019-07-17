115403167edc258f811bacde2474608ca22d8e15
Yanndeo <yan2sambou@gmail.com>
2019-07-17 02:12:28 +0100

    creation d'un nouvel element

--- src/Controller/ListingController.php
+++ src/Controller/ListingController.php
@@ -6,20 +6,18 @@ use App\Entity\Listing;
 use App\Repository\ListingRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 /**
- * @Route("/")
+ * @Route("/" , name="task_")
  */
 class ListingController extends AbstractController
 {
 
 
-    public function __construct()
-    {
-
-    }
 
     /**
      * @Route("/")
@@ -31,5 +29,27 @@ class ListingController extends AbstractController
        return $this->render('listing.html.twig', compact('listings'));
 
     }
+
+    /**
+     * @Route("/new", methods="POST" , name="create")
+     */
+    public function create(EntityManagerInterface $entityManager, Request $request):Response
+    {
+
+        $name = $request->get('name');
+        //var_dump($name); die();
+
+        $listing = new Listing();
+        $listing->setName($name);
+
+        $entityManager->persist($listing);
+        $entityManager->flush();
+
+        return new Response('ok');
+
+    }
+
+
+
 
 }
